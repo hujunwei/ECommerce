36368af8b60a00fcec82f326442699cd1d7ea5b7
Junwei-Desktop-work <hujunwei0614@gmail.com>
2020-03-11 16:51:00 -0700

    Finish API

---
+++ ECommerce.API/Model/ApiProduct.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Model
+{
+    public class ApiProduct
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+        [JsonProperty("Price")]
+        public double Price { get; set; }
+        [JsonProperty("IsAvailability")]
+        public bool IsAvailability { get; set; }
+    }
+}
